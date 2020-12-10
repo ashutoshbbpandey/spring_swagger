9e6e27f5d96adcbbc43998b857c6d06b83ca3579
Ashutosh Pandey <ashutoshbbpandey@gmail.com>
2020-12-10 21:36:17 +0530

    Basic project structure
    Basic project setup

---
+++ src/test/java/com/spring/swagger/spring_swagger_demo/SpringSwaggerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.swagger.spring_swagger_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
